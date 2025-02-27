dd0f3743f1fefcd724433977cc6e73206887e300
Toni Gregov <Toni.Gregov@zenitel.com>
2025-02-27 10:49:35 +0100

    Update the wampsharp missing file

---
+++ src/IntegrationModule/WampSharp/WampSharp/Core/Logs/LogErrorExtensions.cs
@@ -0,0 +1,18 @@
+#pragma warning disable CS1591
+using System;
+
+namespace WampSharp.Logging
+{
+    public static class LogErrorExtensions
+    {
+        public static void ErrorFormat(this ILog log, Exception ex, string message, params object[] arguments)
+        {
+            log.Log(LogLevel.Error, () => message, ex, arguments);
+        }
+
+        public static void Error(this ILog log, string message, Exception ex)
+        {
+            log.Log(LogLevel.Error, () => message, ex);
+        }
+    }
+}
